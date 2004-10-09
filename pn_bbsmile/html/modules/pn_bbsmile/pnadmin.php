651ed0ebf9c6fc7f7538add76d859653c90caa50
landseer <landseer@a854b309-0615-0410-bc97-d51225b3739b>
2004-10-09 07:00:24 +0000

    configuration now pnrendered

--- pn_bbsmile/html/modules/pn_bbsmile/pnadmin.php
+++ pn_bbsmile/html/modules/pn_bbsmile/pnadmin.php
@@ -23,73 +23,29 @@
 // To read the license please visit http://www.gnu.org/copyleft/gpl.html
 // ----------------------------------------------------------------------
 // Original Author of file: Hinrich Donner
-// changed to pn_bbsmile: larsneo
+// changed to pn_bbsmile: larsneo, pnRendered by landseer
 // ----------------------------------------------------------------------
 
 
 // show admin choices
 //
-function pn_bbsmile_admin_main() {
-
-    // get an authkey and the current settings
-    //
-    $authid     = pnSecGenAuthKey();
-    $imagepath  = pnModGetVar('pn_bbsmile', 'smiliepath');
-
-    // make an output
-    //
-    $result = "<align=\"center\" class=\"pn-normal\">"._PNBBSMILE_ADMIN_TITLE."\n"
-        ."<align=\"center\" class=\"pn-normal\">"._PNBBSMILE_ADMIN_TITLE_CONFIG."\n";
-
-    // How about some securety?
-    //
+function pn_bbsmile_admin_main() 
+{
     if (!pnSecAuthAction(0, 'pn_bbsmile::', '::', ACCESS_ADMIN)) {
-        $result .= "<p class=\"pn-normal\">"._PNBBSMILE_ADMIN_NOACCESS."</p>\n";
-        return $result;
+        return _PNBBSMILE_ADMIN_NOACCESS;
     }
 
-    // do we have an status message pending?
-    //
-    $status = pnGetStatusMsg();
-    if (!empty($status)) {
-        $result .= "<p class=\"pn-normal\"><hr noshade />".pnVarPrepForDisplay($status)."<hr noshade /></p>\n";
+    $submit = pnVarCleanFromInput('submit');
+    if(!$submit) {   
+        $pnr =& new pnRender('pn_bbsmile');
+        $pnr->assign('imagepath', pnModGetVar('pn_bbsmile', 'smiliepath'));
+        return $pnr->fetch('pn_bbsmile_admin_main.html');
+    } else {
+        $imagepath = pnVarCleanFromInput('imagepath');
+        pnModSetVar('pn_bbsmile', 'smiliepath', $imagepath);
+        pnSessionSetVar('statusmsg', pnVarPrepForDisplay(_PNBBSMILE_ADMIN_CONFIGSAVED));
+        pnRedirect(pnModURL('pn_bbsmile', 'admin'));
     }
-
-    // show the little form
-    //
-    $result .= "<p class=\"pn-normal\">"._PNBBSMILE_ADMIN_V1_HINT."</p>\n"
-        ."<p class=\"pn-normal\">"
-        ."<form action=\"".pnModURL(pnModGetName(), 'admin', 'setconfig')."\" method=\"post\">\n"
-        ."<input type=\"hidden\" name=\"authid\" value=\"$authid\">\n"
-        ._PNBBSMILE_ADMIN_LABEL_IMAGEPATH.": "
-        ."<input type=\"text\" name=\"imagepath\" value=\"".pnVarPrepForDisplay($imagepath)."\" size=\"40\" maxlength=\"255\"><br />\n"
-        ."<font class=\"pn-sub\">"._PNBBSMILE_ADMIN_HINT_IMAGEPATH."<br /><br />\n"
-        ."<input type=\"submit\" value=\""._PNBBSMILE_ADMIN_BTN_SUBMIT."\"></form></p>\n";
-
-    // that's all folks
-    //
-    return $result;
 }
-
-// do save the changes
-//
-function pn_bbsmile_admin_setconfig() {
-
-    // check auth
-    //
-    if (!pnSecConfirmAuthKey()) {
-        pnSessionSetVar('errormsg', _BADAUTHKEY);
-        pnRedirect(pnModURL(pnModGetName(), 'admin'));
-        return true;
-    }
-
-    // save var
-    //
-    $imagepath = pnVarCleanFromInput('imagepath');
-    pnModSetVar('pn_bbsmile', 'smiliepath', $imagepath);
-    pnSessionSetVar('statusmsg', _PNBBSMILE_ADMIN_CONFIGSAVED);
-    pnRedirect(pnModURL(pnModGetName(), 'admin'));
-}
-
 
 ?>
