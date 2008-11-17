fa7a9f40154ad29ee2ad39ad51cdda2b3a4f3c24
Landseer <Landseer@a854b309-0615-0410-bc97-d51225b3739b>
2008-11-17 20:32:37 +0000

    added italian lang files, credits to Arthens!

---
+++ bbsmile/html/modules/bbsmile/pnlang/ita/common.php
@@ -0,0 +1,108 @@
+<?php
+// $Id$
+// ----------------------------------------------------------------------
+// Zikula Application Framework
+// Copyright (C) 2001 by the Zikula Development Team.
+// http://www.zikula.org/
+// ----------------------------------------------------------------------
+// Based on:
+// PHP-NUKE Web Portal System - http://phpnuke.org/
+// Thatware - http://thatware.org/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+// Original Author of file: Hinrich Donner
+// changed to bbsmile: larsneo
+// ----------------------------------------------------------------------
+/**
+ * @link http://code.zikula.org/projects/community-italian/
+ * @translated by Arthens
+ */
+
+
+// include smilie alternative text defines
+Loader::includeOnce('modules/bbsmile/pnlang/ita/smilies.php');
+
+//
+// A
+//
+define('_BBSMILE_ADDHOOK', 'Attiva bbsmile per altri moduli');
+define('_BBSMILE_ADMIN_BTN_CANCEL', 'Annulla');
+define('_BBSMILE_ADMIN_BTN_SUBMIT', 'Applica');
+define('_BBSMILE_ADMIN_CONFIGSAVED', 'la configurazione di bbsmile è stata aggiornata');
+define('_BBSMILE_ADMIN_CONFIRM_READFROMFILESYSTEM', 'Vuoi che gli smile vengano letti direttamente dalla cartella?');
+define('_BBSMILE_ADMIN_EDITEDSMILIESSAVED', 'Gli smile modificati sono stati salvati.');
+define('_BBSMILE_ADMIN_HINT_IMAGEPATH', 'Inserisci la cartella (relativa) dove si trovano gli smile. Nessun / finale o backslash!');
+define('_BBSMILE_ADMIN_HINT_IMAGEPATH_AUTO', 'Inserisci la cartella (relativa) dove si trovano gli smile da aggiungere automaticamente. Tutti i file con estensione jpg, gif, jpeg o png saranno processati. Nessun / finale o backslash!');
+define('_BBSMILE_ADMIN_LABEL_ACTIVATE_AUTO', 'Attiva auto-smile');
+define('_BBSMILE_ADMIN_LABEL_IMAGEPATH', 'Path agli smile');
+define('_BBSMILE_ADMIN_LABEL_IMAGEPATH_AUTO', 'Path agli smile da includere automaticamente');
+define('_BBSMILE_ADMIN_LABEL_READFROMFILESYSTEM', 'Confermando questa operazione tutti gli smile nella directory sottostante verranno aggiunti. Gli smile la cui immagine risulterà mancante verranno cancellati. I cambiamenti delle informazioni degli smile verranno mantenuti.');
+define('_BBSMILE_ADMIN_LABEL_READSMILIESFROMFILESYSTEM', 'Leggi gli smile dal filesystem');
+define('_BBSMILE_ADMIN_LABEL_REMOVE_INACTIVE', 'Rimuovi le scorciatoie per gli smile inattivi');
+define('_BBSMILE_ADMIN_LABEL_SMILIETABLE', 'Smile attuali');
+define('_BBSMILE_ADMIN_SMILIESREADFROMFILESYSTEM', 'Gli smile sono stati letti dal filesystem.');
+define('_BBSMILE_ADMIN_SMILIETABLE_ACTIVE', 'Attivo');
+define('_BBSMILE_ADMIN_SMILIETABLE_ALIAS', 'Alias per questo smile');
+define('_BBSMILE_ADMIN_SMILIETABLE_ALIAS_HINT', 'Separa gli alias con ",". Non usare spazi.');
+define('_BBSMILE_ADMIN_SMILIETABLE_ALT', 'testo alternativo');
+define('_BBSMILE_ADMIN_SMILIETABLE_FILENAME', 'Nome del file');
+define('_BBSMILE_ADMIN_SMILIETABLE_SHORT', 'Scorciatoia/Trigger');
+define('_BBSMILE_ADMIN_SMILIETABLE_SMILIE', 'Smile');
+define('_BBSMILE_ADMIN_START', 'Pagina Iniziale');
+define('_BBSMILE_ADMIN_TITLE', 'Amministrazione bbsmile');
+define('_BBSMILE_ADMIN_TITLE_CONFIG', 'Configurazione');
+define('_BBSMILE_ADMIN_TITLE_EDITSMILIES', 'Modifica Smile');
+define('_BBSMILE_ADMIN_TITLE_READSMILIESFROMFILESYSTEM', 'Leggi Smile dalla Cartella');
+
+//
+// C
+//
+define('_BBSMILE_CLICKOUTSIDETOCLOSE', 'Clicca fuori dalla finestra per chiuderla');
+define('_BBSMILE_COULDNOTREGISTER', 'bbsmile non è stato installato!');
+define('_BBSMILE_COULDNOTUNREGISTER', 'bbsmile non è stato rimosso!');
+
+//
+// F
+//
+define('_BBSMILE_FORCERELOAD', 'Forza la rilettura degli smile, tutti i dati verranno sovrascritti!');
+
+//
+// G
+//
+define('_BBSMILE_GOTOHOMEPAGE', 'visita la pagina del progetto bbsmile');
+
+//
+// I
+//
+define('_BBSMILE_ILLEGALSMILIEPATH', 'Il path non esiste o il sistema non riesce a leggerlo.');
+define('_BBSMILE_ISHOOKEDWITH', 'Attualmente bbsmile è utilizzato dai seguenti moduli');
+
+//
+// M
+//
+define('_BBSMILE_MORESMILIES', 'Altri Smile');
+
+//
+// N
+//
+define('_BBSMILE_NOAUTOSMILIES', 'Extended Smilies non è ancora stato attivato!');
+define('_BBSMILE_NOSCRIPTWARNING', 'Javascript è disabilitato oppure non è supportato dal tuo browser. L\'interfaccia di bbsmile è stata disattivata.');
+define('_BBSMILE_NOTHOOKED', '** nessun modulo utilizza bbsmile **');
+
+//
+// S
+//
+define('_BBSMILE_SHOWHIDE_SMILIES', 'mostra/nascondi smile');
